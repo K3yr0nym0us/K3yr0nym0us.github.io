4c8cb5c14786b32001d73c61a0fe31849703582e
K3yr0nym0us <kevinmartinezbarra@gmail.com>
2022-12-02 17:14:53 -0300

    creando animacion de hidden a visible

---
+++ assets/js/miFormacion.js
@@ -0,0 +1,8 @@
+const titulo = document.querySelector(".titulo__formacion");
+titulo.addEventListener("click", () => {
+    let imgs = document.querySelector("[data-formacion]");
+    imgs.classList.toggle("imgs__formacion__invisible");
+    let masMenos = document.querySelector("[data-mas-menos]");
+    masMenos.classList.toggle("fi-br-plus");
+    masMenos.classList.toggle("fi-br-minus-small");
+});
