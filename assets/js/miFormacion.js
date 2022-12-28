abd5beae436b67422948fb1186cc8fc52d84a3b5
K3yr0nym0us <kevinmartinezbarra@gmail.com>
2022-12-28 10:19:46 -0300

    rediseñando complentamente

--- assets/js/miFormacion.js
+++
@@ -1,9 +0,0 @@
-const titulo = document.querySelector(".titulo__formacion");
-titulo.addEventListener("click", () => {
-    let imgs = document.querySelector("[data-formacion]");
-    imgs.classList.toggle("imgs__formacion__invisible");
-    let masMenos = document.querySelector("[data-mas-menos]");
-    masMenos.classList.toggle("fi-br-plus");
-    masMenos.classList.toggle("fi-br-minus-small");
-    window.location.href = "Index.html#ancla"
-});
