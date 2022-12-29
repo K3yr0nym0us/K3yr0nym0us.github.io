6c9692df1a8171b7e3b7148927b52f86b26c096b
K3yr0nym0us <kevinmartinezbarra@gmail.com>
2022-12-29 12:49:35 -0300

    configurando cambiode estilo a menu en scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,9 @@
 window.addEventListener('scroll', () => {
+    efectoImgsFormacion();
+    changeIdMenu();
+});
+
+const efectoImgsFormacion = () => {
     const element = document.querySelector("[data-formacion]");
     const position = element.getBoundingClientRect();
     if (position.top <= 500 && position.top >= -50) {
@@ -7,6 +12,26 @@ window.addEventListener('scroll', () => {
         element.classList.remove('imgs__formacion');
         element.classList.add('imgs__formacion--hidden');
     };
-});
+};
 
+const changeIdMenu = () => {
+    const menus = document.querySelectorAll('li');
+    const hrs = document.querySelectorAll('hr');
 
+    hrs.forEach( (hr) => {
+        const position = hr.getBoundingClientRect().top;
+        if (position <= 150 && position >= -50) {
+            const id = hr.id;
+            
+            menus.forEach( (menu) => {
+                const capturaMenu = menu.childNodes[0];
+                const containMenu = capturaMenu.innerHTML.toLowerCase();
+                
+                capturaMenu.removeAttribute('id');
+                if (id == containMenu) {
+                    menu.childNodes[0].setAttribute("id", "active");
+                };
+            });
+        };
+    });
+};
